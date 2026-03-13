65df35bc70ab3d51bbece64095934a01df42ebe3
YeraldinPosada <yeposadag@unal.edu.co>
2026-03-13 16:22:56 -0500

    feat Interconexion entre Laravel y Flask (Productos) crud de peticiones en controlador

---
+++ gateway/app/Http/Controllers/FlaskController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class FlaskController extends Controller
+{
+    public function traer_productos(){
+        $response = Http::withHeaders([
+        'Authorization' => env("TOKEN_APIS"),
+        ])->get(env("ENDPOINT_FLASK"));
+        return [
+        'status' => $response->status(),
+        'body' => $response->body(),
+        ];
+    }
+
+    public function crear_producto(Request $request){
+        $response = Http:: withHeaders([
+        'Authorization' => env("TOKEN_APIS"),
+        ])->post(env("ENDPOINT_FLASK"),[
+            "name"=> $request->name,
+            "price" => $request->price,
+            "color"=> $request->color,
+            "stock"=> $request->stock
+        ]);
+        return [
+        'status' => $response->status(),
+        'body' => $response->body(),
+        ];
+        
+    }
+
+    public function actualizar_producto(Request $request, $id){
+    $response = Http::withHeaders([
+        'Authorization' => env("TOKEN_APIS"),
+    ])->put(env("ENDPOINT_FLASK")."/".$id, [
+        "name" => $request->name,
+        "price" => $request->price,
+        "color" => $request->color,
+        "stock" => $request->stock
+    ]);
+
+    return [
+        'status' => $response->status(),
+        'body' => $response->body(),
+    ];
+
+    } 
+    public function eliminar_producto($id){
+
+    $response = Http::withHeaders([
+        'Authorization' => env("TOKEN_APIS"),
+    ])->delete(env("ENDPOINT_FLASK")."/".$id);
+
+    return [
+        'status' => $response->status(),
+        'body' => $response->body(),
+    ];
+}
+
+}
--- gateway/routes/api.php
+++ gateway/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FlaskController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +13,11 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
 Route::post('/logout', [AuthController::class,'logout'])->middleware('auth:api');
+
+
+//Rutas de productos (Flask)
+Route::get('/productos', [FlaskController::class,'traer_productos']);
+Route::post('/productos', [FlaskController::class,'crear_producto']);
+Route::put('/productos/{id}', [FlaskController::class,'actualizar_producto']);
+Route::delete('/productos/{id}', [FlaskController::class,'eliminar_producto']);
+
